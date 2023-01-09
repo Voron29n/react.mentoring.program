18f0cd666fe6683cca5d5643136d9f08e582dc81
Voron29n <voronovich.pavel99@gmail.com>
2023-01-09 23:04:23 +0300

    ERMP-13 Testing

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -10,11 +10,30 @@ describe('empty spec', () => {
     );
     cy.intercept({ url: 'https://image.tmdb.org/**' }).as('getImage');
     cy.wait('@getMovies');
-    for (let i = 0; i < 30; i++) {
-      cy.wait('@getImage');
-    }
+    expect(cy.wait(Array(10).fill('@getImage')));
+    cy.findAllByRole('img', { timeout: 10000 })
+      .filter('.poster')
+      .should('have.length', 9);
+  });
+
+  it('Search with MovieDetail', () => {
+    cy.intercept('http://localhost:4000/movies?limit=40&offset=0').as(
+      'getMovies'
+    );
+    cy.intercept({ url: 'https://image.tmdb.org/**' }).as('getImage');
+    cy.wait('@getMovies');
+    cy.findAllByRole('img', { timeout: 10000 })
+      .filter('.poster')
+      .should('have.length', 9);
+
     cy.get('.search__container input').type('test');
     cy.get('.search__container button').click();
-    cy.findAllByRole('img').filter('.poster').eq(0).click();
+
+    const poster = cy
+      .findAllByRole('img', { timeout: 10000 })
+      .filter('.poster');
+    poster.should('have.length', 1);
+    poster.eq(0).click();
+    expect(cy.get('.movie__detail', { timeout: 10000 }));
   });
 });
